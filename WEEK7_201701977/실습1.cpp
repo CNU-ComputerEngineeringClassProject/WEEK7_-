20d123f671372792238c51ffb9d1a66e114e4d37
Kuuuna98 <yunaghgh@naver.com>
2020-10-18 22:32:32 +0900

    Modify 실습1 annotation

--- WEEK7_201701977/실습1.cpp
+++ WEEK7_201701977/실습1.cpp
@@ -31,7 +31,7 @@ int main() {
 	}
 }
 /* (실습 1-1) 분석결과 : 
-	main에서 funcA함수가 호출된다. 
+	main의 try문에서 funcA함수가 호출된다. 
 	funcA에서는 id가 A인 Test객체 r을 생성한다. 그리고 funcB함수를 호출한다.
 	funcB에서는 id가 B인 Test객체 r을 생성한다. 그리고 "Executed in B"를 출력하고 funcB함수는 종료된다. 
 	이때 함수가 종료되면서 함수 내부에서 생성된 id가 B인 r객체가 소멸되어야한다. Test의 destructor가 실행되어 "Destructor execution: B"가 출력되고 객체는 소멸한다.
@@ -40,13 +40,16 @@ int main() {
 	funcA함수가 종료되고 함수를 호출했던 상위함수 main으로 돌아간다. 그리고 프로그램이 종료된다. */
 
 /* (실습 1-2) 분석결과 : 
-	main에서 funcA함수가 호출된다. 
+	main의 try문에서 funcA함수가 호출된다. 
 	funcA에서는 id가 A인 Test객체 r을 생성한다. 그리고 funcB함수를 호출한다.
-	funcB에서는 id가 B인 Test객체 r을 생성한다. 그리고 "Exception from funcB!\n"를 인자로 하는 runtime_error type 예외객체가 생성되어 던져진다.(throw된다.)
-	예외가 발생하면 catch문 예외핸들러가 있는 위치로 바로 점프하게 된다. 예외핸들러는 main에 있기때문에 그 위치로 실행흐름이 바뀐다.
+	funcB에서는 id가 B인 Test객체 r을 생성한다. 그리고 "Exception from funcB!\n"를 인자로 하는 runtime_error type 예외객체가 생성되어 던져진다.(예외발생)
+	예외가 발생하면 stack unwinding과정을 통해 catch문 예외핸들러가 있는 main으로 실행흐름이 바뀌어야한다.
+
+	stack unwinding은 funcB에서 예외가 발생하면 funcB의 이후 코드를 실행하지않고 바로 그 함수를 호출한 funcA영역으로 예외데이터가 전달하고,
+	funcA함수에서도 예외를 처리하지 못하면 함수를 호출한 main영역으로 예외데이터가 전달되는 과정이다. 이때, 예외데이터를 전달하면 전달한 함수는 종료된다.
+
 	그래서 funcB의 내부에 있는 출력문이 출력되지않고 바로 함수가 종료된다. 
 	함수가 종료되기 때문에 함수 내부에서 생성된 id가 B인 r객체가 소멸되어야한다. Test의 destructor가 실행되어 "Destructor execution: B"가 출력되고 객체는 소멸한다.
 	그리고 funcA도 내부 출력문이 출력되지않고 바로 함수가 종료된다.
 	함수가 종료되기 때문에 함수 내부에서 생성된 id가 A인 r객체가 소멸되어야한다. Test의 destructor가 실행되어 "Destructor execution: A"가 출력되고 객체는 소멸한다.
-	main의 예외핸들러에서 "Exception : Exception from funcB!\n"를 출력하며 예외를 처리한다. 그리고 프로그램이 종료된다.
-	*/
+	main의 예외핸들러에서 "Exception : Exception from funcB!\n"를 출력하며 예외를 처리한다. 그리고 프로그램이 종료된다.*/
