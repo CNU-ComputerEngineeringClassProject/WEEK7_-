23ab5c640f7c8b74507b3907b5f2a1b7e43bfe22
Kuuuna98 <yunaghgh@naver.com>
2020-10-18 21:56:36 +0900

    Write 실습1 annotation & Change 실습2 exception code

--- WEEK7_201701977/실습1.cpp
+++ WEEK7_201701977/실습1.cpp
@@ -12,7 +12,7 @@ private:
 };
 int funcB() {
 	Test r('B');
-	// throw std::runtime_error("Exception from funcB!\n");
+	throw std::runtime_error("Exception from funcB!\n");
 	std::cout << "Executed in B" << std::endl;
 	return 0;
 }
@@ -30,5 +30,23 @@ int main() {
 		std::cout << "Exception : " << e.what();
 	}
 }
-/* (실습 1-1) 분석결과 : */
-/* (실습 1-2) 분석결과 : */
+/* (실습 1-1) 분석결과 : 
+	main에서 funcA함수가 호출된다. 
+	funcA에서는 id가 A인 Test객체 r을 생성한다. 그리고 funcB함수를 호출한다.
+	funcB에서는 id가 B인 Test객체 r을 생성한다. 그리고 "Executed in B"를 출력하고 funcB함수는 종료된다. 
+	이때 함수가 종료되면서 함수 내부에서 생성된 id가 B인 r객체가 소멸되어야한다. Test의 destructor가 실행되어 "Destructor execution: B"가 출력되고 객체는 소멸한다.
+	funcB함수가 종료되고 함수를 호출했던 상위함수 funcA로 돌아간다. 그리고 "Executed in A"를 출력하고 funcA함수는 종료된다. 
+	이때 함수가 종료되면서 함수 내부에서 생성된 id가 A인 r객체가 소멸되어야한다. Test의 destructor가 실행되어 "Destructor execution: A"가 출력되고 객체는 소멸한다. 
+	funcA함수가 종료되고 함수를 호출했던 상위함수 main으로 돌아간다. 그리고 프로그램이 종료된다. */
+
+/* (실습 1-2) 분석결과 : 
+	main에서 funcA함수가 호출된다. 
+	funcA에서는 id가 A인 Test객체 r을 생성한다. 그리고 funcB함수를 호출한다.
+	funcB에서는 id가 B인 Test객체 r을 생성한다. 그리고 "Exception from funcB!\n"를 인자로 하는 runtime_error type 예외객체가 생성되어 던져진다.(throw된다.)
+	예외가 발생하면 catch문 예외핸들러가 있는 위치로 바로 점프하게 된다. 예외핸들러는 main에 있기때문에 그 위치로 실행흐름이 바뀐다.
+	그래서 funcB의 내부에 있는 출력문이 출력되지않고 바로 함수가 종료된다. 
+	함수가 종료되기 때문에 함수 내부에서 생성된 id가 B인 r객체가 소멸되어야한다. Test의 destructor가 실행되어 "Destructor execution: B"가 출력되고 객체는 소멸한다.
+	그리고 funcA도 내부 출력문이 출력되지않고 바로 함수가 종료된다.
+	함수가 종료되기 때문에 함수 내부에서 생성된 id가 A인 r객체가 소멸되어야한다. Test의 destructor가 실행되어 "Destructor execution: A"가 출력되고 객체는 소멸한다.
+	main의 예외핸들러에서 "Exception : Exception from funcB!\n"를 출력하며 예외를 처리한다. 그리고 프로그램이 종료된다.
+	*/
--- WEEK7_201701977/실습2.cpp
+++ WEEK7_201701977/실습2.cpp
@@ -3,18 +3,19 @@ bool funcA() {
 	int c;
 	std::cout << "input(number) : ";
 	std::cin >> c;
-	if (c < 10) //Let's assume this is exception
-		return false;
-
+	if (c < 10){
+		throw std::out_of_range("Invalis Input!!");
+	}
 	return true;
 }
 int main() {
 
-	if (funcA())
-	{
+	try {
+		funcA();
 	}
-	else {
+	catch (std::out_of_range& e) {
 		std::cout << "Exception : Invalid Input!!" << std::endl;
 	}
+	
 	return 0;
 }
